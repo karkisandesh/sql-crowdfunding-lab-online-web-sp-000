9fd81e2435c7ab559bd917bbb4f3eb192ecc9580
Sandesh Karki <karkisandesh@hotmail.com>
2020-01-16 06:50:05 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,6 +1,12 @@
 CREATE TABLE project
-( title TEXT,
+(title TEXT,
   category TEXT,
   funding_goal TEXT,
   start_date INTEGER,
   end_date INTEGER);
+  
+CREATE TABLE user
+(name TEXT,
+  age INTEGER);
+    
+    
