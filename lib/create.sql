c3716a1d26787e4d2719de0bc3009a4579a4e1af
Sandesh Karki <karkisandesh@hotmail.com>
2020-01-16 06:51:48 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,16 +1,19 @@
 CREATE TABLE project
-(title TEXT,
+(id INTEGER PRIMARY KEY,
+  title TEXT,
   category TEXT,
   funding_goal TEXT,
   start_date INTEGER,
   end_date INTEGER);
   
 CREATE TABLE user
-(name TEXT,
+(id INTEGER PRIMARY KEY,
+  name TEXT,
   age INTEGER);
     
 CREATE TABLE pledge
-(amount INTEGER,
+(id INTEGER PRIMARY KEY,
+  amount INTEGER,
  user_id INTEGER,
  project_id INTEGER
 );
